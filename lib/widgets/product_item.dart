5b8137a875e1716530745308c52b43c1ef34df21
Giuliano Peretti <giuliano.peretti@csttech.it>
2020-11-15 16:38:12 +0100

    Capitolo 187

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,10 +8,14 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(product.imageUrl, fit: BoxFit.cover),
-      footer: GridTileBar(title: Text(product.title, textAlign: TextAlign.center), backgroundColor: Colors.black54, 
-      leading: IconButton(icon: Icon(Icons.favorite), onPressed: () {  },), trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {  },),
-    ));
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+          child: GridTile(
+        child: Image.network(product.imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(title: Text(product.title, textAlign: TextAlign.center), backgroundColor: Color.fromARGB(180, 0, 0, 0), 
+        leading: IconButton(icon: Icon(Icons.favorite_border), onPressed: () {  }, 
+        color: Theme.of(context).accentColor,), trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {  }, color: Theme.of(context).accentColor),
+      )),
+    );
   }
 }
