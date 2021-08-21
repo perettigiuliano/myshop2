e60c3402f6358795933630f7f6b7c761922f01cf
Giuliano Peretti <giuliano.peretti@codeland.it>
2021-08-22 00:06:21 +0200

    Capitolo 225

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class EditProductsScreen extends StatefulWidget {
   static const String ROUTE = "/edit-product";
@@ -10,11 +11,30 @@ class EditProductsScreen extends StatefulWidget {
 class _EditProductsScreenState extends State<EditProductsScreen> {
   // final _priceFocusNode = FocusNode();
   // final _descriptionFocusNode = FocusNode();
+  final _imageURLFocusNode = FocusNode();
+  final _imageURLController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _imageURLFocusNode.addListener(_updateImageURL);
+  }
+
+  void _updateImageURL() {
+    if (!_imageURLFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
 
   @override
   void dispose() {
     // _priceFocusNode.dispose();
     // _descriptionFocusNode.dispose();
+    // _descriptionFocusNode.dispose();
+    _imageURLFocusNode.dispose();
+    _imageURLController.dispose();
+    _imageURLFocusNode.removeListener(_updateImageURL);
+    _imageURLFocusNode.dispose();
     super.dispose();
   }
 
@@ -51,6 +71,39 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                 keyboardType: TextInputType.multiline,
                 // focusNode: _descriptionFocusNode,
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(top: 8, right: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    )),
+                    child: _imageURLController.text.isEmpty
+                        ? Text("Enter URL")
+                        : FittedBox(
+                            child: Image.network(_imageURLController.text),
+                            fit: BoxFit.cover,
+                            clipBehavior: Clip.hardEdge),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "Imgae URL"),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageURLController,
+                      onEditingComplete: () {
+                        setState(() {});
+                      },
+                      focusNode: _imageURLFocusNode,
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
