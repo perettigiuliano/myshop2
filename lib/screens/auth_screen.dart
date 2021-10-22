65a7ca55dbabd1abad7fe9e63968edb3a1bfa041
Giuliano Peretti <giuliano.peretti@codeland.com>
2021-10-23 00:21:00 +0200

    Capitolo 283

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -252,18 +252,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints:
-              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-              BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
-        ),
+      child: AnimatedContainer(
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+        duration: Duration(milliseconds: 300),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
